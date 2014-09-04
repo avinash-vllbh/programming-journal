01720f55916c41860127a901e8f67ebb974dfd3e
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-09-04 13:35:56 -0700

    kth to the last element

---
+++ ruby/LinkedList/KthLastElement.rb
@@ -0,0 +1,43 @@
+# Write an algorithm to find the kth to last element of a singly linked list. [CTCI, 77]
+
+require_relative 'SingleLinkedList'
+class KthLastElement < SinglyLinkedList
+  ###
+  # Creates a LinkedList of random size using Ruby's Psuedo Random number generator
+  # Iterates from 0 to size-1 and add's a random number to the list on self
+  ###
+  require 'pry'
+  def createRandomList
+    list_size = rand(9) + 1
+    list_size.times do
+      add(rand(10))
+    end
+    print
+  end
+
+  ###
+  # Starts with two pointers both pointing to head, we move pointer2 k positions with pointer1 still pointing to the head
+  # After moving pointer2 k positions then we move both pointer1 and pointer2 one position each until we reach the end
+  # At this state pointer1 will be pointing to kth to last position and pointer2 to the end of list
+  # Time complexity O(n) => traverse the list only once
+  # Space complexity O(n) => Axiliary space is just two nodes throughout the execution of the program
+  ###
+  def kthLastElement(k)
+    node1 = head
+    node2 = head
+    k.times do 
+      node2 = node2.next
+      return "List size less than #{k}" if node2 == nil
+    end
+    # binding.pry
+    while node2 != nil
+      node2 = node2.next
+      node1 = node1.next
+    end
+    return node1.value
+  end
+end
+
+knode = KthLastElement.new
+knode.createRandomList
+puts knode.kthLastElement(3)
