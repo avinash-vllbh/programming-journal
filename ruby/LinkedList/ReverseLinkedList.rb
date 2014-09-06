61e5dd20dea0cc7193782315a104cabe2adac222
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-09-05 17:38:19 -0700

    Added reverse LL using recursion

--- ruby/LinkedList/ReverseLinkedList.rb
+++ ruby/LinkedList/ReverseLinkedList.rb
@@ -1,11 +1,14 @@
 #Reverse a linked list
 require_relative 'SingleLinkedList'
 class ReverseLinkedList < SingleLinkedList
+
   # Reverse a linked list iteratively
   def reverseList
     prev = nil
     current = head
     next_node = nil
+    # Reversing the links iteratively
+    # 8 -> 9 => 8 <- 9
     while current != nil
       next_node = current.next
       current.next = prev
@@ -13,26 +16,33 @@ class ReverseLinkedList < SingleLinkedList
       current = next_node
     end
     @head = prev
-    print
   end
 
-  #Reverse a linked list recursively
-  def reverseRecursiveList(head)
-    first = Node.new
-    next_list = Node.new
-    return if head == nil
-    first = head
-    rest = first.next
-    return if rest == nil
-    reverseRecursiveList(rest)
-    first.next =nil
-    rest.next = first
-    return rest
+  ###
+  # Reverse a linked list recursively
+  # Uses function stack during execution to reverse the nodes
+  # Time complexity: O(n)
+  ###
+  def reverseRecursiveList(node)
+    # After reaching end of list we point head to end node and return
+    if node.next == nil
+      @head = node
+      return
+    end
+    reverseRecursiveList(node.next)
+    # Recursion starts folding up
+    temp = node.next
+    temp.next = node
+    node.next = nil
   end
 end
+
 list = ReverseLinkedList.new
-list.createRandomList(10)
+list.createRandomList(7)
 list.print
 list.reverseList
-list.head = list.reverseRecursiveList(list.head)
+puts "List reversed iteratively"
+list.print
+list.reverseRecursiveList(list.head)
+puts "List reversed back to original state recursively"
 list.print
