66c115de72c162d13071f71734c311aed9c78765
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-09-14 07:03:53 -0700

    min_max of an array

---
+++ ruby/Arrays/min_max_array.rb
@@ -0,0 +1,15 @@
+class Minmax
+  attr_accessor :min ,:max
+  def initialize(min = nil ,max = nil)
+    @min = min
+    @max = max
+  end
+end
+def minmax_linear(array)
+  if array.size <= 1
+
+end
+
+def minmax_pairs
+
+end
