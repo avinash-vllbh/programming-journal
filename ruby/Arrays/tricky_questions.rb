35a5735a5a572761344cbdbdb0da916c16d23298
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-09-16 07:42:19 -0700

    worked on array rotations

--- ruby/Arrays/tricky_questions.rb
+++ ruby/Arrays/tricky_questions.rb
@@ -22,12 +22,12 @@ def print_duplicates(array)
     end
   end
 end
+# print_duplicates([1,5,3,4,3,2,3])
 
-print_duplicates([1,5,3,4,3,2,3])
 
 # Find the least positive element missing from given array of size n
-# E.g Array - [5,7,10,-2,4,5,-1,1,2] 
-# Least missing positive element is 3
+#                   E.g Array - [5,7,10,-2,4,5,-1,1,2] 
+#                   Least missing positive element is 3
 # Time complexity: O(n)
 # Space complexity: O(1)
 require 'pry'
@@ -62,6 +62,67 @@ def min_positive_missing_element(array)
   end
   puts "Least positive missing element is #{array.size+1}"
 end
+# min_positive_missing_element([-5,7,10,-2,4,5,-1,1,2,0]) #Will print 3
+# min_positive_missing_element([0,0,1,5,3,4,-6,2,7]) #Will print 6
 
-min_positive_missing_element([-5,7,10,-2,4,5,-1,1,2,0]) #Will print 3
-min_positive_missing_element([0,0,1,5,3,4,-6,2,7]) #Will print 6
+
+
+###
+# Rotate a one dimensional array of n elements by k steps
+#       Input sample = [a,b,c,d,e,f,g] ==>Rotate by 3 steps ==> output = [e,f,g,a,b,c] 
+# The way it works is to reverse the array once and then reverse the two halfs based on number of steps of rotation
+# Time complexity: O(n)
+# Space complexity: O(1) 
+def rotate_array(array,k,type="right")
+  puts "Input array is"
+  p array
+  reverse_array(array, 0, array.size-1)
+  if type == "right"
+    reverse_array(array,0,k-1)
+    reverse_array(array,k,array.size-1)
+  else
+    reverse_array(array,0,k)
+    reverse_array(array,k+1,array.size-1)
+  end
+  puts "#{type} rotated array is"
+  p array
+end
+
+def reverse_array(array, start, finish)
+  while start < finish
+    array[start], array[finish] = array[finish], array[start]
+    start = start.succ
+    finish = finish.pred
+  end
+end
+rotate_array([4,7,3,5,6],2)
+rotate_array([4,7,3,5,6],2,"left")
+rotate_array(["a","b","c","d","e","f","g"],2)
+
+###
+# Rotate a one dimensional array of n elements by k steps
+#       Input sample = [a,b,c,d,e,f,g] ==>Rotate by 3 steps ==> output = [e,f,g,a,b,c] 
+# work in progress
+# Break point doesn't work right if the number of elements are odd
+
+def rotate_array_k_steps(array, k)
+  n = array.size
+  puts "Input array is"
+  p array
+  shifts = (n.to_f/k).ceil
+  # binding.pry
+  current = nil
+  for i in 0..k-1
+    current = array[i] if current.nil?
+    nxt = (i+k)%n
+    shifts.times do
+      temp = array[nxt]
+      array[nxt] = current
+      current = temp
+      nxt = (nxt+k)%n
+    end
+  end
+  puts "Rotated array is"
+  p array
+  p current
+end
