b4e8e717e662b6a91ceb777cc8947660ba2d8749
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-10-28 10:02:19 -0700

    added mark and his toys challenge

--- ruby/hacker_rank/arrays_sorting/mark_and_toy.rb
+++ ruby/hacker_rank/arrays_sorting/mark_and_toy.rb
@@ -1,1 +1,16 @@
-https://www.hackerrank.com/challenges/mark-and-toys
+# https://www.hackerrank.com/challenges/mark-and-toys
+# Read number of toys and money with mark from STDIN
+# Read price of each toy into toys array
+n, money = gets.split(" ").map(&:to_i)
+prices = gets.split(" ").map(&:to_i).sort # Covert each price to integer and sort the array
+
+toys = 0
+prices.each do |price|
+  if money - price >= 0
+    money = money - price
+    toys += 1
+  else
+    break
+  end
+end
+puts toys
